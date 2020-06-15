a627d62d98f7a96f332401d1874f91b6f411de99
tmkadamcz <tmkadamcz@gmail.com>
2020-06-15 09:53:49 -0700

    Initial

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+Get distribution from percentiles
+
+First, choose a distribution family
+
+2-parameter distributions
+	normal
+	log-normal
+	beta
+
+meta-logistic distribution.
+
+If you provide more than two percentiles for a 2-parameter distribution.
+least squares will be used for fitting.
+'''
+
+'''
+Get percentiles from distribution
+
+Choose a distribution family and provide its parameters
+(I can reuse code from bayes-continuous for this)
+'''
