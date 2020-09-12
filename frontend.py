9102c1e542334b4e326c4770b5b490d2af1db3e6
tmkadamcz <tmkadamcz@gmail.com>
2020-09-11 20:53:27 -0700

    ripping out metalog

--- frontend.py
+++ frontend.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from wtforms import SelectField, StringField, FloatField, FormField, validators, BooleanField
-import secrets
 import backend
 import mpld3
 import os
